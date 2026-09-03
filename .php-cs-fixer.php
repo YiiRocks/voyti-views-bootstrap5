c9126fce08106fd52bc1ec1eb2dfadc5fc29ce6d
Mr. 42 <admin@mr42.me>
2026-09-04 01:11:02 +0200

    Simplify PHP CS Fixer configuration

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -8,19 +8,8 @@ use Yiisoft\CodeStyle\ConfigBuilder;
 $finder = (new Finder())->in(__DIR__);
 
 return ConfigBuilder::build()
-    ->setRiskyAllowed(true)
     ->setUsingCache(false)
     ->setRules([
         '@Yiisoft/Core' => true,
-        '@PER-CS3.0' => true,
-        'ordered_class_elements' => [
-            'sort_algorithm' => 'alpha',
-        ],
-        'ordered_imports' => [
-            'imports_order' => [
-                'const', 'class', 'function',
-            ],
-            'sort_algorithm' => 'alpha',
-        ],
     ])
     ->setFinder($finder);
